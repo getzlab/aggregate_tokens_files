a09342b994d5f7aa8f98af08471a89d1e35c4d87
Julian Hess <jhess@broadinstitute.org>
2021-04-14 01:12:53 +0000

    Dereference symlinks

--- wolF/tasks.py
+++ wolF/tasks.py
@@ -3,6 +3,10 @@ from wolf import Task
 class aggregate_tokens(Task):
     name = "aggregate_tokens"
     inputs = { "token_list", "pon_name" }
-    script = "/app/aggregate_tokens_files ${token_list} ${pon_name}.bin"
+    script = """
+# because MATLAB is braindead and can't resolve symlinks
+while read -r token; do readlink -f $token; done < ${token_list} > token_list_symlinks.txt
+/app/aggregate_tokens_files token_list_symlinks.txt ${pon_name}.bin
+"""
     output_patterns = { "pon_binary" : "*.bin" }
     docker = "gcr.io/broad-getzlab-workflows/aggregate_pon_tokens:v7"
